1363f7ab84d39b15c284ff0fa2d1726480d14925
Leandro Silveira OUTLOOK <leandro.prado@outlook.com.br>
2020-07-15 23:45:46 +0000

    Updated DeployToACI.ps1

--- DeployToACI.ps1
+++ DeployToACI.ps1
@@ -20,4 +20,4 @@ $acrUsername=az acr credential show --name $containerRegistry --resource-group $
 $acrPassword=az acr credential show --name $containerRegistry --resource-group $resourceGroup --query passwords[0].value
 $acrloginserver = $(az acr show --name $containerRegistry --query loginServer)
 
-az container create --resource-group $resourceGroup --name $containerName --image $containerImage --os-type $osType --cpu $cpus --memory $memory --registry-login-server $acrloginserver --registry-username $acrUsername --registry-password $acrPassword --dns-name-label $appDns --ports $appPort --environment-variables $environmentVariables
+az container create --resource-group $resourceGroup --name $containerName --image $containerImage --os-type $osType --cpu $cpus --memory $memory --registry-login-server $acrloginserver --registry-username $acrUsername --registry-password $acrPassword --dns-name-label $appDns --ports $appPort
